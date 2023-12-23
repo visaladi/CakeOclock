6fbbdf6e9dac2d8adfb69dc818208ce3c36d6852
S. T. Jayaweera <jayaweera_st_e22@engug.ruh.ac.lk>
2023-12-24 00:23:15 +0530

    published the app

--- TangyWeb_API/Program.cs
+++ TangyWeb_API/Program.cs
@@ -93,12 +93,13 @@ var app = builder.Build();
 StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe")["ApiKey"];
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "CakeOClock API v1");
+    c.RoutePrefix = String.Empty;
+});
 app.UseHttpsRedirection();
 app.UseCors("Tangy");
 
