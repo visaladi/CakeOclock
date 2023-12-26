bc31275f5f794d66c9c522ab4c5e1269b4bc794a
S. T. Jayaweera <jayaweera_st_e22@engug.ruh.ac.lk>
2023-12-26 15:08:37 +0530

    making custom order page

---
+++ BlazorFileUpload.Shared/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorFileUpload.Shared
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ BlazorFileUpload.Shared/UploadResult.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorFileUpload.Shared
+{
+    public class UploadResult
+    {
+        public int Id { get; set; }
+        public string? FileName { get; set; }
+        public string? StoredFileName { get; set; }
+        public string? ContentType { get; set; }
+    }
+} 
