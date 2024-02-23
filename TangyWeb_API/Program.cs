6e326fcc32f90af1d6995b8038886bd789c6a587
SupunJayaweera <jayaweera_st_e22@engug.ruh.ac.lk>
2024-02-23 15:42:12 +0530

    Modified the landing page

--- TangyWeb_API/Program.cs
+++ TangyWeb_API/Program.cs
@@ -104,21 +104,21 @@ StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe")["ApiKey"
 
 // Configure the HTTP request pipeline.
 
-//app.UseSwagger();
-//app.UseSwaggerUI(c =>
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//});
-
-//deployment
-
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "CakeOClock API v1");
-    c.RoutePrefix = String.Empty;
+    app.UseSwagger();
+    app.UseSwaggerUI();
 });
+
+//deployment
+
+//app.UseSwagger();
+//app.UseSwaggerUI(c =>
+//{
+//    c.SwaggerEndpoint("/swagger/v1/swagger.json", "CakeOClock API v1");
+//    c.RoutePrefix = String.Empty;
+//});
 
 app.UseHttpsRedirection();
 app.UseCors("Tangy");
