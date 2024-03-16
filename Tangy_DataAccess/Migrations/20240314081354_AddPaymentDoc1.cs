9c80f6316c464da71c0602c2d7313c01e04e898f
SupunJayaweera <jayaweera_st_e22@engug.ruh.ac.lk>
2024-03-16 14:42:49 +0530

    Backup for another file upload

---
+++ Tangy_DataAccess/Migrations/20240314081354_AddPaymentDoc1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tangy_DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPaymentDoc1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "paymentDocs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FileName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StoredFileName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ContentType = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_paymentDocs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "paymentDocs");
+        }
+    }
+}
