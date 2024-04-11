2b998bebae109448d5a909d1160007689ab4eae6
SupunJayaweera <jayaweera_st_e22@engug.ruh.ac.lk>
2024-04-11 16:39:05 +0530

    solving validation errors

--- Tangy_Models/OrderHeaderDTO.cs
+++ Tangy_Models/OrderHeaderDTO.cs
@@ -32,19 +32,25 @@ namespace Tangy_Models
         [Display(Name = "Name")]
         [Required]
         public string Name { get; set; }
+
         [Required]
         [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
+
         [Required]
         [Display(Name = "Street Address")]
         public string StreetAddress { get; set; }
+
         [Required]
         public string State { get; set; }
+
         [Required]
         public string City { get; set; }
+
         [Required]
         [Display(Name = "Postal Code")]
         public string PostalCode { get; set; }
+
         [Display(Name = "Email")]
         [Required]
         public string Email { get; set; }
