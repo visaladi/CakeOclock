2a3a162f456cd9735bfdeaaead14f0b14a883d2c
SupunTJ <130586987+SupunJayaweera@users.noreply.github.com>
2024-04-21 19:00:47 +0530

    adding code separation to Index page

---
+++ TangyWeb_Client/Pages/Index.razor.cs
@@ -0,0 +1,26 @@
+﻿using Syncfusion.Blazor;
+
+namespace TangyWeb_Client.Pages
+{
+    public partial class Index
+    {
+        private string? activeBreakpoint { get; set; }
+        public bool IsProcessing { get; set; } = false;
+        private List<MediaBreakpoint> mediaQuery { get; set; } = new List<MediaBreakpoint>()
+    {
+     new MediaBreakpoint() { Breakpoint = "Iphone", MediaQuery = "(max-width: 500px)" },
+     new MediaBreakpoint() { Breakpoint = "Desktop", MediaQuery = "(min-width: 1400px)" },
+     new MediaBreakpoint() { Breakpoint = "Ipad", MediaQuery = "(min-width: 900px)" },
+     new MediaBreakpoint() { Breakpoint = "Tablet", MediaQuery = "(min-width: 500px)" }
+    };
+
+
+
+        protected override async Task OnInitializedAsync()
+        {
+            IsProcessing = true;
+
+            IsProcessing = false;
+        }
+    }
+}
