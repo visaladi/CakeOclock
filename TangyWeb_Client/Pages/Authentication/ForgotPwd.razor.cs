251fb43c2b31e08504de0974dc857d044eae16c7
SupunTJ <jayaweeraspn@gmail.com>
2024-05-09 07:43:07 +0530

    formating the code in Auth directory

---
+++ TangyWeb_Client/Pages/Authentication/ForgotPwd.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.JSInterop;
+using Tangy_Models;
+
+namespace TangyWeb_Client.Pages.Authentication
+{
+    public partial class ForgotPwd
+    {
+        private ResetPasswordDTO resetModel = new ResetPasswordDTO();
+        private bool ShowErrors;
+        private string Error = "";
+
+        private async Task HandleForgotPassword()
+        {
+            ShowErrors = false;
+            var result = await AuthService.ForgotPassword(resetModel);
+
+            if (!result.Successful)
+            {
+                await JS.InvokeVoidAsync("ShowSwal", "error", "Task Faild!");
+                Error = result.Error!;
+                ShowErrors = true;
+            }
+            else
+            {
+
+                await JS.InvokeVoidAsync("ShowSwal", "success", "Password reset link has been sent to your email, please check it out!");
+                Error = result.Error!;
+                ShowErrors = true;
+            }
+        }
+    }
+}
---
+++ TangyWeb_Client/Pages/Authentication/ResetPwd.razor.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.JSInterop;
+using Tangy_Models;
+
+namespace TangyWeb_Client.Pages.Authentication
+{
+    public partial class ResetPwd
+    {
+        public string? Email;
+        public string? Token;
+
+        private string Message = string.Empty;
+        ResetPasswordDTO resetPasswordDTO = new ResetPasswordDTO();
+        private bool ShowErrors;
+        private string? Errors;
+
+        private async Task HandleResetPassword()
+        {
+            ShowErrors = false;
+            Message = "Please Wait...";
+
+            var uri = NavigationManager.ToAbsoluteUri(NavigationManager.Uri);
+            var queryStrings = QueryHelpers.ParseQuery(uri.Query);
+            if (queryStrings.TryGetValue("email", out var email))
+            {
+                Email = email;
+            }
+            if (queryStrings.TryGetValue("token", out var token))
+            {
+                Token = token;
+            }
+
+            resetPasswordDTO.Email = email;
+            resetPasswordDTO.Token = token;
+
+            var result = await AuthService.ResetForgotPassword(resetPasswordDTO);
+
+            if (result.Successful)
+            {
+                await JS.InvokeVoidAsync("ShowSwal", "success", "You Have Reset the Password Successfully!");
+                NavigationManager.NavigateTo("/login");
+            }
+            else
+            {
+                await JS.InvokeVoidAsync("ShowSwal", "error", "Task Faild!");
+                Errors = "Something went wrong.";
+                ShowErrors = true;
+            }
+        }
+    }
+}
