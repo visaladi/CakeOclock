54e86cbca7594d0bb362ba228afdbb191ab3702e
SupunTJ <jayaweeraspn@gmail.com>
2024-06-21 11:41:18 +0530

    Adding user management

---
+++ TangyWeb_Server/Service/UserService.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Tangy_DataAccess.Data;
+
+namespace TangyWeb_Server.Service
+{
+    public class UserService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UserService(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<List<IdentityUser>> GetUsersAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<IdentityUser> GetUserByIdAsync(string userId)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Id == userId);
+        }
+
+        public async Task<bool> UpdateUserAsync(IdentityUser user)
+        {
+            var existingUser = await _userManager.FindByIdAsync(user.Id);
+            if (existingUser == null)
+                return false;
+
+            existingUser.UserName = user.UserName;
+            existingUser.Email = user.Email;
+            existingUser.PhoneNumber = user.PhoneNumber;
+
+            var result = await _userManager.UpdateAsync(existingUser);
+            return result.Succeeded;
+        }
+
+        public async Task<bool> DeleteUserAsync(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+                return false;
+
+            var result = await _userManager.DeleteAsync(user);
+            return result.Succeeded;
+        }
+    }
+}
